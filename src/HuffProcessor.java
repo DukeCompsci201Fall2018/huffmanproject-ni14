638e7db2e2e3065fd12519465afa68f9b9986e5a
Nishant Iyengar <iyengar.nish@gmail.com>
2018-12-05 17:45:34 -0500

    hi

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -42,11 +42,14 @@ public class HuffProcessor {
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
 
+
+
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
-		}
+		 }
+
 		out.close();
 	}
 	/**
@@ -60,11 +63,71 @@ public class HuffProcessor {
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		int bits = in.readBits(BITS_PER_INT);
+
+		if(bits != HUFF_TREE){
+			throw new HuffException("illegal header starts with " + bits);
 		}
+
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
+
+
+		//while (true){
+		//	int val = in.readBits(BITS_PER_WORD);
+		//	if (val == -1) break;
+		//	out.writeBits(BITS_PER_WORD, val);
+		//}
+
+
 		out.close();
 	}
+
+
+	public HuffNode readTreeHeader(BitInputStream in){
+
+		int bit = in.readBits(1);
+		if (bit == -1){
+			throw new HuffException("weird");
+		}
+		if (bit == 0) {
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0,0,left,right);
+		}
+		else {
+			int value = in.readBits(BITS_PER_WORD + 1);
+			return new HuffNode(value,0,null,null);
+		}
+
+
+	}
+
+	public void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out){
+
+		HuffNode current = root;
+
+		while (true) {
+			int bits = in.readBits(1);
+			if (bits == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if (bits == 0) current = current.myLeft;
+				else current = current.myRight;
+
+				if (current.myLeft == null && current.myRight == null) {
+					if (current.myValue == PSEUDO_EOF)
+						break;   // out of loop
+					else {
+						out.writeBits(BITS_PER_WORD, current.myValue);
+						current = root; // start back after leaf
+					}
+				}
+			}
+		}
+
+	}
+
+	
 }
