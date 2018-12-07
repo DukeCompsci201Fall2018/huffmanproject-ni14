0cec8cc230f6776f76c565584602c02dde801f56
Varun Nair <nairvarun18@gmail.com>
2018-12-06 20:54:00 -0500

    Added compression part of Huffman

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  * Although this class has a history of several years,
@@ -41,17 +42,123 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
-
-
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		 }
-
+//		while (true){
+//			int val = in.readBits(BITS_PER_WORD);
+//			if (val == -1) break;
+//			out.writeBits(BITS_PER_WORD, val);
+//		 }
+//
+//		out.close();
+		
+		int[] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+		
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+		
+		in.reset();
+		writeCompressedBits(codings, in, out);
 		out.close();
 	}
+	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
+		
+		while(true) {
+			int bits = in.readBits(BITS_PER_WORD);
+			
+			if(bits==-1) {
+				String code = codings[PSEUDO_EOF];
+				out.writeBits(code.length(), Integer.parseInt(code, 2));
+				break;
+			} else {
+				String code = codings[bits];
+				out.writeBits(code.length(), Integer.parseInt(code, 2));
+			}
+		}
+		
+		out.close();
+
+	}
+
+	private void writeHeader(HuffNode root, BitOutputStream out) {
+		
+		if (root.myLeft == null && root.myRight == null) {
+			out.writeBits(BITS_PER_WORD + 1, 1 + root.myValue);
+		}
+		else {
+			out.writeBits(BITS_PER_WORD + 1, 0);
+			writeHeader(root.myLeft, out);
+			writeHeader(root.myRight, out);
+		}
+	}
+
+	private String[] makeCodingsFromTree(HuffNode root) {
+		// TODO Auto-generated method stub
+		
+		String[] encodings = new String[ALPH_SIZE + 1];
+		codingHelper(root, "", encodings);
+		return encodings;
+	}
+
+	private void codingHelper(HuffNode root, String path, String[] encodings) {
+		// TODO Auto-generated method stub
+		if(root.myLeft==null && root.myRight==null) {
+			encodings[root.myValue] = path;
+		}
+		else {
+			codingHelper(root.myLeft, path + "0", encodings);
+			codingHelper(root.myRight, path + "1", encodings);
+		}
+		
+	}
+
+	private HuffNode makeTreeFromCounts(int[] counts) {
+		// TODO Auto-generated method stub
+		
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+		
+		for(int i = 0; i < counts.length; i++){
+			if(counts[i] > 0) {
+				pq.add(new HuffNode(i, counts[i], null, null));
+			}
+		}
+		
+		while(pq.size() > 1) {
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			
+			HuffNode t = new HuffNode(left.myWeight+right.myWeight, left.myWeight+right.myWeight, left, right);
+			pq.add(t);
+		}
+		
+		HuffNode root = pq.remove();
+		
+		return root;
+	}
+
+	/**
+	 * Method that determines frequencies of each eight-bit character in the file.
+	 * @param in
+	 * @return freq - int[] containing frequencies.
+	 */
+	private int[] readForCounts(BitInputStream in) {
+		
+		// Initialize new array that contains freqs of all characters and PSEUDO_EOF
+		int[] freq = new int[ALPH_SIZE+1];
+		
+		while (true){
+		int val = in.readBits(BITS_PER_WORD);
+		if (val == -1) break;
+		
+		// If val != -1, then increment freq in array by 1.
+		freq[val] = freq[val] + 1;
+		
+		}
+		freq[PSEUDO_EOF] = 1;
+
+		return freq;
+	}
+
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
@@ -71,6 +178,7 @@ public class HuffProcessor {
 
 		HuffNode root = readTreeHeader(in);
 		readCompressedBits(root, in, out);
+		out.close();
 
 
 		//while (true){
@@ -80,7 +188,6 @@ public class HuffProcessor {
 		//}
 
 
-		out.close();
 	}
 
 
@@ -113,12 +220,22 @@ public class HuffProcessor {
 				throw new HuffException("bad input, no PSEUDO_EOF");
 			}
 			else {
+				
+				// Using 0 and 1 to traverse Huffman Tree
+				// If 0, go left.
 				if (bits == 0) current = current.myLeft;
+				
+				// If 1, go right.
 				else current = current.myRight;
 
+				// If we have arrived at a leaf...
 				if (current.myLeft == null && current.myRight == null) {
+					
+					// Break out of loop if stop character is reached.
 					if (current.myValue == PSEUDO_EOF)
 						break;   // out of loop
+					
+					// Otherwise write value of current leaf and restart at root.
 					else {
 						out.writeBits(BITS_PER_WORD, current.myValue);
 						current = root; // start back after leaf
